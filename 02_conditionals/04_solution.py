2d4f10002c8eaa1027cadc8bd9dd46c496890d4d
Ravikant Mahto <157304763+ravikantmahto2410@users.noreply.github.com>
2025-10-28 18:26:56 +0530

    Fruit ripeness checker better solution

--- 02_conditionals/04_solution.py
+++ 02_conditionals/04_solution.py
@@ -1,12 +1,34 @@
-banana_color = "Brown"
 
-if banana_color == 'green':
-    fruit = "Unripe"
+######## Solved by Myself 
+# banana_color = "Brown"
 
-elif banana_color == 'Yellow':
-    fruit = "Ripe"
+# if banana_color == 'green':
+#     fruit = "Unripe"
+
+# elif banana_color == 'Yellow':
+#     fruit = "Ripe"
+
+# else:
+#     fruit = "Overripe"
+
+# print("Your Fruit is :- ", fruit)
+
+
+
+# ###############Solution Given by the Hitesh Sir
+
+fruit  = "Apple"
+color = "Green"
+
+if fruit == "Banana":
+    if color == "Green":
+        print("Unripe")
+    
+    elif color == "Yellow":
+        print("Ripe")
+
+    elif color == "Brown":
+        print("Overripe")
 
 else:
-    fruit = "Overripe"
-
-print("Your Fruit is :- ", fruit)
+    print("Sufficient Information of Fruit is not provided")
