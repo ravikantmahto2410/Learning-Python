034e14c46387b6f9a438ef826625ee2e29bebb57
Ravikant Mahto <157304763+ravikantmahto2410@users.noreply.github.com>
2025-10-28 18:46:41 +0530

    Transportation mode selection question is solved

---
+++ 02_conditionals/06_solution.py
@@ -0,0 +1,11 @@
+distance = 100
+
+if distance < 3:
+    mode = "Walk"
+
+elif distance < 15:
+    mode = "Bike"
+else :
+    mode = "Car"
+
+print(" Your mode of transportation should be :-", mode)
