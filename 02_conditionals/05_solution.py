7d2dab046d7383060a612b6f3a1479659417045d
Ravikant Mahto <157304763+ravikantmahto2410@users.noreply.github.com>
2025-10-28 18:37:48 +0530

    Weather activity suggestion problem solved

---
+++ 02_conditionals/05_solution.py
@@ -0,0 +1,11 @@
+weather = "Rainy"
+
+if weather == "Sunny":
+    activity = "Go for a Walk"
+elif weather == "Rainy":
+    activity = "Read a book"
+
+elif weather == "Snowy":
+    activity = "Build a Snowman"
+
+print("Your activity is predicted :-", activity)
