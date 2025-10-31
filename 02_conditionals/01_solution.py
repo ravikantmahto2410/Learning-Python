00e4c2faa3ca60bb12fffe8f5313405ef03ca1cc
Ravikant Mahto <157304763+ravikantmahto2410@users.noreply.github.com>
2025-10-31 18:44:19 +0530

    Solved the question Find the First Non-Repeated Character

--- 02_conditionals/01_solution.py
+++ 02_conditionals/01_solution.py
@@ -1,6 +1,6 @@
 # userage = 25;
 #  we can also take input like  userage = input("Enter the user age : ")
-# we can also convert the entered value into the user there itself like this age = int(input("Provide me an age : "))
+# we can also convert the entered value into the integer there itself like this age = int(input("Provide me an age : "))
 #  we can also do like for more readability
 #       age = input("Provide me an age : ")
 #       age_in_int = int(age)
---
+++ 03_loops/05_solution.py
@@ -0,0 +1,15 @@
+# input_str = "teeter"
+
+# for char in input_str:
+#     if input_str.count(char) == 1:
+#         print("Char is : ", char)
+
+
+####### Method- 01 Optimised
+
+input_str = "teeteracdacd"
+
+for char in input_str:
+    if input_str.count(char) == 1:
+        print("Char is : ", char)
+        break
