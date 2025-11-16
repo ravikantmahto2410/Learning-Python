5f58f82b42b12777b1326fc0fb86e6c90fd868c2
Ravikant Mahto <157304763+ravikantmahto2410@users.noreply.github.com>
2025-11-16 18:39:35 +0530

    Learn the basic function syntax

--- 05_functions/01_solutions.py
+++ 05_functions/01_solutions.py
@@ -1,6 +1,28 @@
+# def square_of_num(number):
+#     print(number ** 2)
+
+# ####Function ho gya hai tyaar
+
+# square_of_num(6)
+
+## now a bit better 
+# def square_of_num(number):
+#     print(number ** 2)
+
+# result = square_of_num(4)
+# print(result)  ###here we get none means empty because
+
+
+# variation 3
+# def square_of_num(number):
+#     return number ** 2
+
+# result = square_of_num(4)
+# print(result)  #### this will return 16
+
+# Variation 4
 def square_of_num(number):
-    print(number ** 2)
+    return number ** 2
 
-####Function ho gya hai tyaar
-
-square_of_num(6)
+result = square_of_num(4)
+print(square_of_num(4))  #### this will return 16 , but this 16 is not returned due to variable result, its because first the square_of_num(4) is evaluated and this returns 16 and then it is printed
