ebba37592145f527fdbc770ca00796d8be172159
Ravikant Mahto <157304763+ravikantmahto2410@users.noreply.github.com>
2025-11-26 17:06:37 +0530

    write the lambda function to calculate the cube of a given number

---
+++ 05_functions/06_solutions.py
@@ -0,0 +1,21 @@
+# Problem: Create a lambda function to compute the cube of a number.
+
+# def add(a, b):
+#     return a + b
+
+# result = add(1,2)
+
+#  in the above function kaam jyaada karna pada, but is this is good if we want to use many times , 
+
+# #### But if we want to use single times itna likhna badi mushkil hai aur kya kuch alag kar sakta hai 
+
+
+#### lambda functions are those functions  jjiska naam nahi hota
+
+# ### Now the question is to write the function to calculate the cube of a number using the lambda function
+
+
+
+cube = lambda x : x ** 3     ##iss poore portion ka koi naam nahi hai, lekin iss poore portion ka jo result hai usko hune store kar liya hai cube ke ander
+print(cube(3)) 
+
