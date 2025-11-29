b71e8a76bb33ff2709ea4ff3dde4d1428f0e9fe9
Ravikant Mahto <157304763+ravikantmahto2410@users.noreply.github.com>
2025-11-29 19:02:12 +0530

    Demonstrate the use of isinstance() to check if my_tesla is an instance of Car and ElectricCar.

---
+++ 07_oop/09_solution.py
@@ -0,0 +1,31 @@
+class Car:
+    total_car = 0
+    def __init__(self, brand, model):
+        self.__brand = brand
+        self.model = model
+        Car.total_car += 1
+
+    def get_brand(self):
+        return self.__brand + "!"
+
+    def full_name(self):
+        return f"{self.__brand} {self.model}"
+    
+    def fuel_type(self): ###3 python mein context to dena hi padta hai , so we are writing the self
+        return "Petrol or Diesel"
+
+    
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_Size):
+        super().__init__(brand,model)
+        self.battery_size  = battery_Size
+
+    def fuel_type(self): ###3 python mein context to dena hi padta hai , so we are writing the self
+        return "Battery"
+
+
+
+myHarrier = ElectricCar("Tata","Harrier.ev", "79kWh")
+print(isinstance(myHarrier, ElectricCar))
+print(isinstance(myHarrier, Car))
